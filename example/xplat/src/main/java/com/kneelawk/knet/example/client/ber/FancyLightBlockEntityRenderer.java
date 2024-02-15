846b74d96e38397305fe62713c3103050a5abfcd
Kneelawk <kneelawk@gmail.com>
2024-02-15 14:18:12 -0800

    Finish example BER

--- example/xplat/src/main/java/com/kneelawk/knet/example/client/ber/FancyLightBlockEntityRenderer.java
+++ example/xplat/src/main/java/com/kneelawk/knet/example/client/ber/FancyLightBlockEntityRenderer.java
@@ -28,28 +28,50 @@ package com.kneelawk.knet.example.client.ber;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 
 import com.kneelawk.knet.example.blockentity.FancyLightBlockEntity;
 
+import static com.kneelawk.knet.example.KNetExample.id;
+
 public class FancyLightBlockEntityRenderer implements BlockEntityRenderer<FancyLightBlockEntity> {
+    private static final Identifier OVERLAY_TEXTURE = id("block/fancy_light_overlay");
+
     public FancyLightBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {}
 
     @Override
     public void render(FancyLightBlockEntity entity, float tickDelta, MatrixStack matrices,
                        VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        Sprite sprite =
+            MinecraftClient.getInstance().getSpriteAtlas(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).apply(OVERLAY_TEXTURE);
+
         VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getCutout());
 
+        int r = entity.getRed();
+        int g = entity.getGreen();
+        int b = entity.getBlue();
 
+        face(consumer, matrices.peek(), 0f, 0f, 1f, 0f, 0f, 0f, 1f, 0f, 0f, 1f, 0f, 1f, sprite, r, g, b, light);
+        face(consumer, matrices.peek(), 0f, 1f, 0f, 0f, 1f, 1f, 1f, 1f, 1f, 1f, 1f, 0f, sprite, r, g, b, light);
+        face(consumer, matrices.peek(), 1f, 1f, 0f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 1f, 0f, sprite, r, g, b, light);
+        face(consumer, matrices.peek(), 0f, 1f, 1f, 0f, 0f, 1f, 1f, 0f, 1f, 1f, 1f, 1f, sprite, r, g, b, light);
+        face(consumer, matrices.peek(), 0f, 1f, 0f, 0f, 0f, 0f, 0f, 0f, 1f, 0f, 1f, 1f, sprite, r, g, b, light);
+        face(consumer, matrices.peek(), 1f, 1f, 1f, 1f, 0f, 1f, 1f, 0f, 0f, 1f, 1f, 0f, sprite, r, g, b, light);
     }
 
-    private void face(VertexConsumer consumer, MatrixStack.Entry entry, float x0, float y0, float z0, float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float u0, float v0, float u1, float v1, int red, int green, int blue, int light) {
+    private void face(VertexConsumer consumer, MatrixStack.Entry entry, float x0, float y0, float z0, float x1,
+                      float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, Sprite sprite,
+                      int red, int green, int blue, int light) {
         Matrix4f model = entry.getPositionMatrix();
         Matrix3f normal = entry.getNormalMatrix();
 
@@ -68,6 +90,13 @@ public class FancyLightBlockEntityRenderer implements BlockEntityRenderer<FancyL
         ny *= div;
         nz *= div;
 
-        consumer.vertex(model, x0, y0, z0).color(red, green, blue, 255).texture(u0, v0).light(light).normal(normal, nx, ny, nz).next();
+        consumer.vertex(model, x0, y0, z0).color(red, green, blue, 255).texture(sprite.getMinU(), sprite.getMinV())
+            .light(light).normal(normal, nx, ny, nz).next();
+        consumer.vertex(model, x1, y1, z1).color(red, green, blue, 255).texture(sprite.getMinU(), sprite.getMaxV())
+            .light(light).normal(normal, nx, ny, nz).next();
+        consumer.vertex(model, x2, y2, z2).color(red, green, blue, 255).texture(sprite.getMaxU(), sprite.getMaxV())
+            .light(light).normal(normal, nx, ny, nz).next();
+        consumer.vertex(model, x3, y3, z3).color(red, green, blue, 255).texture(sprite.getMaxU(), sprite.getMinV())
+            .light(light).normal(normal, nx, ny, nz).next();
     }
 }
