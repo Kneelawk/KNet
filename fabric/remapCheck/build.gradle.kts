c3c747d6975d067d83867d16ac24aa4232ee292f
Kneelawk <kneelawk@gmail.com>
2024-05-03 23:17:20 -0700

    Update remapcheck

--- fabric/remapCheck/build.gradle.kts
+++ fabric/remapCheck/build.gradle.kts
@@ -31,8 +31,8 @@ plugins {
 remapCheck {
     val minecraft_version: String by project
     val yarn_version: String by project
+    applyTargetMapping("net.fabricmc:yarn:$minecraft_version+build.$yarn_version:v2")
     checkRemap {
         targetProject(":fabric")
-        targetMapping("net.fabricmc:yarn:$minecraft_version+build.$yarn_version:v2")
     }
 }
